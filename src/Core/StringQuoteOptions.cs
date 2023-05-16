79cf2f976862749f3cd1da4afc881200f2174a28
Ross King <ross.king@altavec.com>
2023-05-16 13:20:02 +1000

    ♻️ remove unused usings

--- src/Core/StringQuoteOptions.cs
+++ src/Core/StringQuoteOptions.cs
@@ -5,11 +5,6 @@
 // -----------------------------------------------------------------------
 
 namespace Altemiq;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 /// <summary>
 /// The string quoting options.
