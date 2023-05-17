b797cf47adea371a23050f24f04314d643821b35
Ross King <ross.king@altavec.com>
2023-05-17 15:11:05 +1000

    ✅ add `IndexOf` tests

---
+++ src/Tests/Core.Tests/Linq/ListTests.IndexOf.cs
@@ -0,0 +1,23 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ListTests.IndexOf.cs" company="Altemiq">
+// Copyright (c) Altemiq. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Altemiq.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public partial class ListTests
+{
+    [Theory]
+    [MemberData(nameof(GetLists))]
+    public void IndexOf(object first, object second)
+    {
+        TestListList<int, int>(first, second, (f, s) => f.IndexOf(s), 1).Should().Be(1);
+        TestReadOnlyListReadOnlyList<int, int>(first, second, (f, s) => f.IndexOf(s), 1).Should().Be(1);
+    }
+}
