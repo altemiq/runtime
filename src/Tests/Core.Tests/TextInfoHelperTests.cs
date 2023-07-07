f353cff653b866bfb6069fe2ba5495f9321b08b3
Ross King <ross.king@altavec.com>
2023-07-07 15:46:18 +1000

    ✅ add text info helper tests

---
+++ src/Tests/Core.Tests/TextInfoHelperTests.cs
@@ -0,0 +1,34 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="TextInfoHelperTests.cs" company="Altemiq">
+// Copyright (c) Altemiq. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Altemiq;
+
+using System.Globalization;
+
+public class TextInfoHelperTests
+{
+    [Fact]
+    public void GetInfoFromCultureInfo()
+    {
+        var cultureInfo = CultureInfo.CurrentCulture;
+        TextInfoHelper.GetInstance(cultureInfo).Should().Be(cultureInfo.TextInfo);
+    }
+
+    [Fact]
+    public void GetInfoFromFormatProvider()
+    {
+        var cultureInfo = CultureInfo.CurrentCulture;
+        var numberFormatProvider = NumberFormatInfo.GetInstance(cultureInfo);
+        TextInfoHelper.GetInstance(numberFormatProvider).Should().Be(cultureInfo.TextInfo);
+    }
+
+    [Fact]
+    public void GetInfoFromNull()
+    {
+        var cultureInfo = CultureInfo.CurrentCulture;
+        TextInfoHelper.GetInstance(default).Should().Be(cultureInfo.TextInfo);
+    }
+}
