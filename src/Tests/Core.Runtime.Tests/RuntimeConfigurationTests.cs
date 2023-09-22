6e27c63421af1bbe4872e6f6c24c7c349a77daf3
Ross King <ross.king@altavec.com>
2023-09-22 12:54:22 +1000

    ✅ add runtime tests

---
+++ src/Tests/Core.Runtime.Tests/RuntimeConfigurationTests.cs
@@ -0,0 +1,24 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="RuntimeConfigurationTests.cs" company="Altavec">
+// Copyright (c) Altavec. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Altavec.Runtime;
+
+public class RuntimeConfigurationTests
+{
+    [Fact]
+    public void AddLibraryPath()
+    {
+        RuntimeConfiguration.AddLibraryRuntimeFolder();
+        Environment.GetEnvironmentVariable("PATH").Should().Contain(RuntimeInformation.GetRuntimeLibraryPath());
+    }
+
+    [Fact]
+    public void AddNativePath()
+    {
+        RuntimeConfiguration.AddNativeRuntimeFolder();
+        Environment.GetEnvironmentVariable("PATH").Should().Contain(RuntimeInformation.GetRuntimeNativePath());
+    }
+}
---
+++ src/Tests/Core.Runtime.Tests/RuntimeInformationTests.cs
@@ -0,0 +1,19 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="RuntimeInformationTests.cs" company="Altavec">
+// Copyright (c) Altavec. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Altavec.Runtime;
+
+public class RuntimeInformationTests
+{
+    [Fact]
+    public void GetRuntimeIdentifier() => RuntimeInformation.RuntimeIdentifier.Should().NotBeNullOrEmpty();
+
+    [Fact]
+    public void GetRuntimeNativePath() => RuntimeInformation.GetRuntimeNativePath().Should().NotBeNull();
+
+    [Fact]
+    public void GetRuntimeLibraryPath() => RuntimeInformation.GetRuntimeLibraryPath().Should().NotBeNull();
+}
