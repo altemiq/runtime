f7193d7bbcb7b703d1e6c151d8bbfbe4bed94374
Ross King <ross.king@altavec.com>
2024-12-10 09:26:11 +1100

    ✅ add more protobuf tests

---
+++ src/Tests/Protobuf.Tests/Converters/StructConverterTests.cs
@@ -0,0 +1,81 @@
+﻿namespace Altavec.Protobuf.Converters;
+
+public class StructConverterTests
+{
+    [Fact]
+    public void CreateStructFromJson()
+    {
+        var json = """
+            {
+              "Number": 1,
+              "Double": 123.456,
+              "False": false,
+              "True": true,
+              "Null": null,
+              "String": "value",
+              "Nested": {
+                "Id": 1
+              },
+              "Array": [
+                1,
+                2,
+                3,
+                4,
+                5
+              ]
+            }
+            """;
+
+        var document = System.Text.Json.JsonDocument.Parse(json);
+        var @struct = StructConverter.ToStruct(document);
+        @struct.Should().NotBeNull();
+    }
+
+    [Fact]
+    public void CreateStructFromNull()
+    {
+        var document = System.Text.Json.JsonDocument.Parse("null");
+        var @struct = StructConverter.ToStruct(document);
+        @struct.Should().BeNull();
+    }
+
+    [Fact]
+    public void CreateJsonFromStruct()
+    {
+        var @struct = new Google.Protobuf.WellKnownTypes.Struct
+        {
+            Fields =
+            {
+                { "Number", Google.Protobuf.WellKnownTypes.Value.ForNumber(1) },
+                { "Double", Google.Protobuf.WellKnownTypes.Value.ForNumber(123.456) },
+                { "False", Google.Protobuf.WellKnownTypes.Value.ForBool(false) },
+                { "True", Google.Protobuf.WellKnownTypes.Value.ForBool(true) },
+                { "Null", Google.Protobuf.WellKnownTypes.Value.ForNull() },
+                { "String", Google.Protobuf.WellKnownTypes.Value.ForString("value") },
+                {
+                    "Nested",
+                    Google.Protobuf.WellKnownTypes.Value.ForStruct(
+                        new Google.Protobuf.WellKnownTypes.Struct
+                        {
+                            Fields =
+                            {
+                                {"Id", Google.Protobuf.WellKnownTypes.Value.ForNumber(1) }
+                            },
+                        })
+                },
+                {
+                    "Array",
+                    Google.Protobuf.WellKnownTypes.Value.ForList(
+                        Google.Protobuf.WellKnownTypes.Value.ForNumber(1),
+                        Google.Protobuf.WellKnownTypes.Value.ForNumber(2),
+                        Google.Protobuf.WellKnownTypes.Value.ForNumber(3),
+                        Google.Protobuf.WellKnownTypes.Value.ForNumber(4),
+                        Google.Protobuf.WellKnownTypes.Value.ForNumber(5))
+                        
+                }
+            }
+        };
+
+        @struct.ToJsonDocument().Should().NotBeNull();
+    }
+}
---
+++ src/Tests/Protobuf.Tests/WellKnownTypes/UUidTests.cs
@@ -0,0 +1,35 @@
+namespace Altavec.Protobuf.WellKnownTypes;
+
+public class UUidTests
+{
+    [Fact]
+    public void FromGuid()
+    {
+        var guid = Guid.NewGuid();
+        Uuid.ForGuid(guid).ToGuid().Should().Be(guid);
+    }
+
+    [Fact]
+    public void Descriptor() => Uuid.Descriptor.Should().NotBeNull();
+
+    [Fact]
+    public void Parser() => Uuid.Parser.Should().NotBeNull();
+
+    [Fact]
+    public void NewUuid() => Uuid.NewUuid().Should().NotBeNull();
+
+#if NET9_0_OR_GREATER
+    [Fact]
+    public void FromGuidVersion7()
+    {
+        var guid = Guid.CreateVersion7();
+        Uuid.ForGuid(guid).ToGuid().Should().Be(guid);
+    }
+
+    [Fact]
+    public void CreateVersion7() => Uuid.CreateVersion7().Should().NotBeNull();
+
+    [Fact]
+    public void CreateVersion7WithTimestamp() => Uuid.CreateVersion7(DateTimeOffset.UtcNow).Should().NotBeNull();
+#endif
+}
