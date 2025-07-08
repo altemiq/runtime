7db3b158f78631bdfb3599b088b21172cc442da2
Ross King <rsking@gmail.com>
2025-07-09 09:41:02 +1000

    🔨 update check version scripts

--- check.version.cmd
+++ check.version.cmd
@@ -1,10 +1,7 @@
-dotnet tool install altemiq.semanticversioning --global
-  & dotnet tool update altemiq.semanticversioning --global
+dotnet tool update altemiq.semanticversioning --global
 dotnet build src
 dotnet semver diff solution src^
   --output BreakingChanges^
-  --source https://nuget.pkg.github.com/altemiq/index.json ^
   --direct-download^
   --version-suffix personal^
-  --package-id-regex Altemiq^
-  --package-id-replace Mondo
+  --force
--- check.version.sh
+++ check.version.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 
-dotnet tool install altemiq.semanticversioning --global --add-source https://nuget.pkg.github.com/altemiq/index.json \
-	&& dotnet tool update altemiq.semanticversioning --global --add-source https://nuget.pkg.github.com/altemiq/index.json
+dotnet tool update altemiq.semanticversioning --global
 dotnet build src
 dotnet semver diff solution src \
   --output BreakingChanges \
-  --source https://nuget.pkg.github.com/altemiq/index.json \
   --direct-download \
-  --version-suffix personal \
-  --package-id-regex Altemiq \
-  --package-id-replace Mondo
+  --version-suffix personal
+  --force
